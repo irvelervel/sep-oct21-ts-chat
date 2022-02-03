6d13e3aba5182d7039c8d8050c10ca51c7b39bf5
Stefano Casasola <ste.casasola@gmail.com>
2022-02-03 11:52:57 +0100

    Users list is always up-to-date

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,8 @@
 import { FormEvent, useEffect, useState } from 'react'
 import { Col, Container, Row, Form, FormControl, ListGroup } from 'react-bootstrap'
 import { io } from 'socket.io-client'
+import IMessage from '../types/message'
+import IUser from '../types/user'
 
 // STEPS HAPPENING
 // 1) io establishes the connection to the server
@@ -12,6 +14,11 @@ import { io } from 'socket.io-client'
 // 7) if the server receives the username correctly, will repond us back with a "loggedin" event
 // 8) the client sets also a bear trap (an event listener with .on()) for that "loggedin" event and prints a message in the console
 // 9) in the meanwhile we're keeping a "loggedIn" state variable up-to-date, so we can enable/disable input fields
+// 10) when a user succesfully logs in, he/she will fetch the list of already connected clients with fetchOnlineUsers
+// 11) the other clients, when already logged-in, will receive an event of "newConnection" when another clients connects in the meanwhile
+// 12) this event of "newConnection" is going to tell them to re-fetch the list of online users, to remain in sync (also when a client disconnects)
+// 13) once a user logs in with a valid username, he/she will be able to send a message, emitting a newMessage event
+// 14) this event will carry over a message object
 
 const ADDRESS = 'http://localhost:3030'
 const socket = io(ADDRESS, { transports: ['websocket'] })
@@ -21,7 +28,7 @@ const Home = () => {
   const [username, setUsername] = useState('')
   const [message, setMessage] = useState('')
   const [loggedIn, setLoggedIn] = useState(false)
-  const [onlineUsers, setOnlineUsers] = useState([])
+  const [onlineUsers, setOnlineUsers] = useState<IUser[]>([])
 
   // socket.io (both FE&BE) is a event-driven library!
   // the first event that happens when a client connects to the server
@@ -45,6 +52,15 @@ const Home = () => {
       // submission went well
       console.log('username successfully registered! :)')
       setLoggedIn(true)
+      fetchOnlineUsers()
+      // moving the newConnection event listener here makes sure a not-yet
+      // logged in user will not be able to receive the newConnection event :)
+      socket.on('newConnection', () => {
+        // newConnection is an event sent to all the OTHER clients
+        // but the one which just connected
+        console.log('a new user just connected!')
+        fetchOnlineUsers()
+      })
     })
   }, [])
 
@@ -56,6 +72,41 @@ const Home = () => {
     // how can I know if my username at this point has been sent successfully?
     // because in that case, the server is programmed to send me an event back :)
     // so in this case I should receive back an event of type "loggedin"
+  }
+
+  const fetchOnlineUsers = async () => {
+    try {
+      let response = await fetch(ADDRESS + '/online-users')
+      if (response.ok) {
+        // let { onlineUsers }: { onlineUsers: IUser[] } = await response.json()
+        // data is an object with onlineUsers into it, which is the array of users
+        let data = await response.json()
+        console.log(data)
+        let onlineUsers: IUser[] = data.onlineUsers
+        setOnlineUsers(onlineUsers)
+      } else {
+        console.log('error retrieving online users')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleSubmitMessage = (e: FormEvent) => {
+    e.preventDefault()
+    // message properties:
+    // text <- the content of the message, a string
+    // sender <- the sender of the message, a string
+    // timestamp: <- the timestamp, a number
+    // id <- the id of this client-server connection, a string
+    const newMessage: IMessage = {
+      text: message,
+      sender: username,
+      id: socket.id,
+      timestamp: Date.now(),
+    }
+
+    socket.emit('sendmessage', newMessage)
   }
 
   return (
@@ -81,7 +132,7 @@ const Home = () => {
             <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
           </ListGroup>
           {/* NEW MESSAGE AREA */}
-          <Form onSubmit={() => {}}>
+          <Form onSubmit={handleSubmitMessage}>
             <FormControl
               placeholder="What's your message?"
               value={message}
@@ -94,11 +145,10 @@ const Home = () => {
           {/* CONNECTED CLIENTS COLUMN */}
           <div className='mb-3'>Connected users:</div>
           <ListGroup>
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            {onlineUsers.length === 0 && <ListGroup.Item>No users yet</ListGroup.Item>}
+            {onlineUsers.map((user) => (
+              <ListGroup.Item key={user.id}>{user.username}</ListGroup.Item>
+            ))}
           </ListGroup>
         </Col>
       </Row>
